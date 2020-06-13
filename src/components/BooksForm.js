e802984da6780ccc8449f7ff0310bc3ab26c1c77
ibtechraj <raj_shk@rediffmail.com>
2020-06-13 19:00:01 +0530

    added category dropdown

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addBook } from "../actions";
+import CATEGORIES from "../utils/categories";
 
 const AddBook = ({ dispatch }) => {
   let input;
@@ -20,8 +21,29 @@ const AddBook = ({ dispatch }) => {
           category.value = "";
         }}
       >
-        <input ref={node => (input = node)} />
-        <input ref={node => (category = node)} />
+        {/* <input ref={node => (input = node)} />
+        <input ref={node => (category = node)} /> */}
+        <input
+          type="text"
+          name="title"
+          // value={title}
+          // onChange={this.handleChange}
+          placeholder="Book Title"
+        />
+        <select
+          className="twenty-five"
+          name="category"
+          value={category}
+          // onChange={this.handleChange}
+          id=""
+        >
+          <option defaultValue="">Choose a category</option>
+          {CATEGORIES.map(category => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
         <button type="submit">Add Book</button>
       </form>
     </div>
